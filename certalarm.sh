05551a617ad3520279f681b8800b648aceb260f1
Filipe Guarino <filipe.maciel@sysmap.com.br>
2020-05-07 11:48:37 -0300

    just fix identation

--- certalarm.sh
+++ certalarm.sh
@@ -12,7 +12,7 @@ FuncReq() {
         else
 	    yum -y install mutt 1> /dev/null 2> /dev/null
             cat mutt.cfg > ~/.muttrc
-    fi
+        fi
 }
 
 FuncMailNotif() {
@@ -22,14 +22,14 @@ FuncMailNotif() {
 	fi
 	local EXPIRED=0
 	if [ $DAYS_TO_EXPIRE -ge $EXPIRED ]; then
-		echo "send mail"
-		echo "DAYS_TO_EXPIRE=$(($DAYS_TO_EXPIRE -1))" > expiration.txt
-		for MAILADDR in $(cat mailto.txt); do
-		    echo "[CERT_ALARM] the certificate will expire in $DAYS_TO_EXPIRE days" | mutt -s "[CERT_ALARM] Warning! Certificate will expire soon!!" -- $MAILADDR
-	    	echo "[$(date +%d%m%Y)] [CERT_ALARM] mail sent to $MAILADDR" >> /var/log/messages
-	    	sleep 3
-	    done
-	    echo "[$(date +%d%m%Y_%H%M)] [CERT_ALARM] the certificate will expire in $DAYS_TO_EXPIRE days" >> /var/log/messages	
+	    echo "send mail"
+	    echo "DAYS_TO_EXPIRE=$(($DAYS_TO_EXPIRE -1))" > expiration.txt
+	    for MAILADDR in $(cat mailto.txt); do
+	        echo "[CERT_ALARM] the certificate will expire in $DAYS_TO_EXPIRE days" | mutt -s "[CERT_ALARM] Warning! Certificate will expire soon!!" -- $MAILADDR
+    	        echo "[$(date +%d%m%Y)] [CERT_ALARM] mail sent to $MAILADDR" >> /var/log/messages
+    	        sleep 3
+            done
+            echo "[$(date +%d%m%Y_%H%M)] [CERT_ALARM] the certificate will expire in $DAYS_TO_EXPIRE days" >> /var/log/messages	
 	fi
 }
 
